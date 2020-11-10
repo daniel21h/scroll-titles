b961aac96c5845114183683e2621fc4f622b8cde
daniel21h <hesseldaniel21@gmail.com>
2020-11-10 13:11:43 -0300

    finalized code

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import streamThumbnail from "../../images/stream_thumbnail.jpg";
+
+import {
+  StreamContainer,
+  StreamThumbnail,
+  StreamColumn,
+  StreamRow,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+  List,
+} from "./styles";
+
+const StreamList: React.FC = () => {
+  const StreamItem: React.FC = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>hesselfive</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            Front-end com Next.js, Chakra UI e GraphQL
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>
+            Science & Technology
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+
+          <TagView>
+            <TagText>Hibrid Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  );
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+};
+
+export default StreamList;
---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../../styles/colors";
+
+import { Container, Heading } from "./styles";
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Container>
+      <Heading>Coming soon...</Heading>
+    </Container>
+  );
+};
+
+export default ComingSoon;
